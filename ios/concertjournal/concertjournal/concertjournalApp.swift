bd36017fc9d64b8c6a84ddd3809036dcdfaedb71
Paul Kühnel <paul.kuehnel27@gmail.com>
2025-12-19 12:05:27 +0100

    feat: add ios app

---
+++ ios/concertjournal/concertjournal/concertjournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  concertjournalApp.swift
+//  concertjournal
+//
+//  Created by Paul Kühnel on 19.12.25.
+//
+
+import SwiftUI
+
+@main
+struct concertjournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/concertjournal/concertjournalTests/concertjournalTests.swift
@@ -0,0 +1,16 @@
+//
+//  concertjournalTests.swift
+//  concertjournalTests
+//
+//  Created by Paul Kühnel on 19.12.25.
+//
+
+import Testing
+
+struct concertjournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
